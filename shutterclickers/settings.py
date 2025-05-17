65bacebdd58fbaca922fc5b89694daaed1711910
Daniel-R-ci <5zhq901m@students.codeinstitute.net>
2025-05-17 17:36:03 +0200

    Update README about deployment

--- shutterclickers/settings.py
+++ shutterclickers/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*q4sp#lv(p4l-yei664hghj6s6eytp9its!w-pq=cf3q(4_1#t'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = bool(os.environ.get("DEBUG_STATUS"))
 
-ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
