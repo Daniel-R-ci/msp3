d0f19ea6d35a9b466a672e272b0637108e783578
Daniel-R-ci <5zhq901m@students.codeinstitute.net>
2025-05-28 15:44:24 +0200

    Collectstatic

--- staticfiles/js/album.js
+++ staticfiles/js/album.js
@@ -4,30 +4,43 @@ $(document).ready(function(){
     // Get the delete modal
     const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 
-    // Hide edit form
+    // Hide forms form
+    $("#addPhotoContainer").hide();
     $("#editAlbumContainer").hide();
+ 
+    // Create event handler to show add photo form
+    $("#showAddPhotoButton").click(function(){
+        $("#addPhotoContainer").show(); 
+        $("#memberButtons").hide();
+    });
 
-    // Create event handler to show form
-    $("#showFormButton").click(function(){
+    // Create event handler to show edit album form
+    $("#showEditButton").click(function(){
         $("#editAlbumContainer").show(); 
-        $("#showFormButton").hide();
-        $("#deleteAlbumButton").hide();
-        albumName = document.getElementById("albumName").innerHTML
-        albumDescription = document.getElementById("albumDescription").innerHTML
+        $("#membberButtons").hide();
+        albumName = document.getElementById("albumName").innerHTML;
+        albumDescription = document.getElementById("albumDescription").innerHTML;
+        console.log(albumName);
+        console.log(albumDescription);
         $("#id_name").val(albumName);
         $("#id_description").val(albumDescription);
-    })
+    });
 
-    // Create event handler for Cancel button
-    $("#cancelEditButton").click(function(){
-        $("showFormButton").show();
-        $("deleteAlbumButton").show();
+ // Create event handler for Cancel button for add photo
+    $("#cancelAddPhotoButton").click(function(event){
+        $("#memberButtons").show();
+        $("#addPhotoContainer").hide();
+    });
+
+    // Create event handler for Cancel button for edit form
+    $("#cancelEditAlbumButton").click(function(event){
+        $("#memberButtons").show();
         $("#editAlbumContainer").hide();
-    })
+    });
 
     // Create event handler for delete button
     $("#deleteAlbumButton").click(function(){
         deleteModal.show();
-    })
+    });
 
 })
--- staticfiles/js/photo.js
+++ staticfiles/js/photo.js
@@ -1,42 +1,68 @@
-const editButtons = document.getElementsByClassName("btn-edit");
-const commentText = document.getElementById("id_comment");
-const commentForm = document.getElementById("commentForm");
-const submitButton = document.getElementById("submitButton");
-const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
-const deleteButtons = document.getElementsByClassName("btn-delete");
-const deleteConfirm = document.getElementById("deleteConfirm");
+$(document).ready(function () {
 
-/**
-* Initializes edit functionality for the provided edit buttons.
-* Code taken from Code Institute I think terefore I blog code along project
-* Modified to suit 
-*/
-for (let button of editButtons) {
-  button.addEventListener("click", (e) => {
-    let commentId = e.target.getAttribute("data-comment_id");
-    let photoId = e.target.getAttribute("data-photo_id");
-    let commentContent = document.getElementById(`comment${commentId}`).innerText;
- 
-    commentText.value = commentContent;
-    submitButton.innerText = "Update";
+    const editButtons = document.getElementsByClassName("btn-edit");
+    const commentText = document.getElementById("id_comment");
+    const commentForm = document.getElementById("commentForm");
+    const submitButton = document.getElementById("submitButton");
 
-    commentForm.setAttribute("action", `${photoId}/edit_comment/${commentId}`);
-  });
-}
+    const deleteButtons = document.getElementsByClassName("btn-delete");
+    const deleteCommentConfirm = document.getElementById("deleteCommentConfirm");
+    const deletePhotoConfirm = document.getElementById("deletePhotoConfirm");
 
+    const deleteCommentModal = new bootstrap.Modal(document.getElementById("deleteCommentModal"));
+    const deletePhotoModal = new bootstrap.Modal(document.getElementById("deletePhotoModal"));
+    
+    /**
+    * Initializes edit functionality for the provided edit buttons.
+    * Code taken from Code Institute I think terefore I blog code along project
+    * Modified to suit 
+    */
+    for (let button of editButtons) {
+        button.addEventListener("click", (e) => {
+            let commentId = e.target.getAttribute("data-comment_id");
+            let photoId = e.target.getAttribute("data-photo_id");
+            let commentContent = document.getElementById(`comment${commentId}`).innerText;
 
+            commentText.value = commentContent;
+            submitButton.innerText = "Update";
 
-/**
-* Initializes deletion functionality for the provided delete buttons.
-* Code taken from Code Institute I think terefore I blog code along project
-* Modified to suit 
-*/
-for (let button of deleteButtons) {
-  button.addEventListener("click", (e) => {
-    let commentId = e.target.getAttribute("data-comment_id");
-    let photoId = e.target.getAttribute("data-photo_id")
-    document.getElementById("submitButton").getAttribute("data-photo_id");
-    deleteConfirm.href = `${photoId}/delete_comment/${commentId}`;
-    deleteModal.show();
-  });
-}
+            commentForm.setAttribute("action", `${photoId}/edit_comment/${commentId}`);
+        });
+    }
+
+    /**
+    * Initializes deletion functionality for the provided delete buttons.
+    * Code taken from Code Institute I think terefore I blog code along project
+    * Modified to suit 
+    */
+    for (let button of deleteButtons) {
+        button.addEventListener("click", (e) => {
+            let commentId = e.target.getAttribute("data-comment_id");
+            let photoId = e.target.getAttribute("data-photo_id")
+            document.getElementById("submitButton").getAttribute("data-photo_id");
+            deleteCommentConfirm.href = `${photoId}/delete_comment/${commentId}`;
+            deleteCommentModal.show();
+        });
+    }
+
+    //Hide edit form
+    $("#editPhotoContainer").hide();
+
+    //Create event handler for edit button
+    $("#showEditButton").click(function(){
+        $("#memberButtons").hide();
+        $("#editPhotoContainer").show();
+    });
+
+    //Create event handler for cancel button
+    $("#cancelEditPhotoButton").click(function (){
+        $("#editPhotoContainer").hide();
+        $("#memberButtons").show();
+    });
+
+    // Create event handler for delete button
+    $("#deletePhotoButton").click(function(){
+        deletePhotoModal.show();
+    });
+
+})
