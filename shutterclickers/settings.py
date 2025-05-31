5eabab816e289c95056938c62528e45e182bff10
Daniel-R-ci <5zhq901m@students.codeinstitute.net>
2025-05-31 17:04:10 +0200

    Change cloudinary settings in settings.py

--- shutterclickers/settings.py
+++ shutterclickers/settings.py
@@ -102,16 +102,6 @@ DATABASES = {
     'default':
     dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-"""
-Deafult sqlite database commented out
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-"""
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -157,6 +147,7 @@ MESSAGE_TAGS = {
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage' # noqa
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
